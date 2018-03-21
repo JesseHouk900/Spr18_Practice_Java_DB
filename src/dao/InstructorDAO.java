7760bb4f3e0a1b0262e26fd0b3eeba9256fb1ba2
Jesse Houk <jessejameshouk@gmail.com>
2018-03-20 20:56:09 -0500

    Changes from Monday, March 19, 2018

--- src/dao/InstructorDAO.java
+++ src/dao/InstructorDAO.java
@@ -15,9 +15,9 @@ public class InstructorDAO {
     private Connection myConn;
 
     public InstructorDAO() throws Exception {
-        String dbURL = "jdbc:mysql://localhost/university";
-        String user = "root";
-        String pass = "root";
+        String dbURL = "jdbc:mysql://cs2.mwsu.edu:3306/houkj";
+        String user = "houkj";
+        String pass = "ustangsM20209447";
         myConn = DriverManager.getConnection(dbURL, user, pass);
         System.out.println("DB connection successful");
     }
